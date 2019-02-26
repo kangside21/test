8940bf0384d697e14d156b9c768ef82228074f7f
kangside21 <kangside21@gmail.com>
2019-02-26 17:14:56 +0900

    커밋 완료함

--- module/route.js
+++ module/route.js
@@ -1,24 +1,11 @@
 (function () {
     var db = require('../db');
     module.exports = function(app) {
-        // app.get('/',function(req,res){
-        //     res.render('index.html')
-        // });
-        
-        app.get('/',(req,res) => {
-            res.render('index.html')
-        });
-        
-        app.get('/about',function(req,res){
-            res.render('index1.html');
+        app.get('/test',(req,res) => {
+            res.render('test.html')
         });
 
         app.get('/search',function(req,res){
-            console.log(req.params);
-            console.log(req.query);
-
-            console.log(db);
-
             db.any("SELECT * FROM TB_NOTICE")
             .then(function (result) {
                 res.status(200).json(result);
@@ -29,15 +16,27 @@
 
         });
 
-        app.post('/search11',function(req,res){
-            console.log(req.body);
-            db.any("SELECT * FROM CODE WHERE CODE='A'")
+        app.get('/search2',function(req,res){
+            console.log(req.query);
+            db.any("SELECT * FROM TB_NOTICE WHERE NOTICE_TITLE = $1", [req.query.title])
             .then(function (result) {
                 res.status(200).json(result);
             })
             .catch(function (error) {
                 res.status(400).json(error);
             });
+
+        });
+
+        app.post('/search3',function(req,res){
+            console.log(req.body);
+            db.any("SELECT * FROM TB_NOTICE WHERE NOTICE_CONTENT LIKE '%' || $1 || '%'", [req.body.content])
+                .then(function (result) {
+                    res.status(200).json(result);
+                })
+                .catch(function (error) {
+                    res.status(400).json(error);
+                });
 
         });
     }
