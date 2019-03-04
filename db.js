18f615f2d7b71e9c1c3f8d84651f6cebf0b9f8da
kangside21 <kangside21@gmail.com>
2019-03-04 13:27:57 +0900

    코밋함

--- db.js
+++ db.js
@@ -2,9 +2,16 @@
   // promise(callback) 처리를 하기 위해서 사용함.
   var promise = require('bluebird');
 
-  // db setting 비번@DB URL
+  // db setting
   var pgp = require("pg-promise")({promiseLib: promise});
-  var conString = "postgres://test:1234@192.168.1.85:5432/test";
-  var db = pgp(conString);
+  var config = {
+    host: '192.168.1.85',
+    port: 5432,
+    database: 'test',
+    user: 'test',
+    password: '1234'
+  };
+  //var conString = "postgres://test:1234@192.168.1.85:5432/test";
+  var db = pgp(config);
   module.exports = db;
 })();
